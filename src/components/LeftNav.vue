d1af821630512b3bd82bf80659cd6e29f38e5158
xanian7 <xanian407@gmail.com>
2023-03-14 14:51:36 -0400

    fixing leftnav button behavior + adding sass

--- src/components/LeftNav.vue
+++ src/components/LeftNav.vue
@@ -1,75 +1,68 @@
 <template>
-    <div class="sidebar">
-        <div class="sidebar-backdrop"></div>
-        <div v-if="!show"
-                 class="shrink-sidebar">
-                <slot>
-                    <button @click="toggleSidebarPanel">-</button>   
-                </slot>
-            </div>
-        <transition name="slide">
-            <div v-if="show"
-                 class="sidebar-panel">
-                <slot>
-                    <button @click="toggleSidebarPanel">-</button>    
-                </slot>
-            </div>
-        </transition>
-    </div>
+	<aside :class="`${is_expanded ? 'is-expanded' : ''}`">
+		<div class="menu-toggle-wrap">
+			<button class="menu-toggle" @click="ToggleMenu">
+				<span class="material-icons">-</span>
+			</button>
+		</div>
+		<div class="flex"></div>
+	</aside>
 </template>
-<script>
-    export default {
-        data: () => ({
-            show: true
-        }),
-        methods: {
-            toggleSidebarPanel() {
-                this.show = !this.show
-            },
-        }
-    }
+
+<script setup>
+import { ref } from 'vue'
+
+const is_expanded = ref(localStorage.getItem("is_expanded") === "true")
+
+const ToggleMenu = () => {
+	is_expanded.value = !is_expanded.value
+	localStorage.setItem("is_expanded", is_expanded.value)
+}
 </script>
-<style>
-    .slide-enter-active,
-    .slide-leave-active {
-        transition: all 0.5s ease;
-        
-    }
 
-    .slide-enter-from,
-    .slide-leave-to {
-        transform: translateX(-75%);
-    }
-
-    .sidebar-backdrop {
-        background-color: rgba(0,0,0,.5);
-        width: 100vw;
-        height: 100vh;
-        position: fixed;
-        top: 0;
-        left: 0;
-        cursor: pointer;
-    }
-    .shrink-sidebar {
-        overflow-y: auto;
-        background-color: #1fa588;
-        position: fixed;
-        left: 0;
-        top: 0;
-        height: 100vh;
-        z-index: 999;
-        padding: 3rem 20px 2rem 20px;
-        width: 75px;
-    }
-    .sidebar-panel {
-        overflow-y: auto;
-        background-color: #1fa588;
-        position: fixed;
-        left: 0;
-        top: 0;
-        height: 100vh;
-        z-index: 999;
-        padding: 3rem 20px 2rem 20px;
-        width: 300px;
-    }
+<style lang="scss" scoped>
+aside {
+	display: flex;
+	flex-direction: column;
+	background-color:rgb(37, 189, 138);
+	color:aquamarine;
+	width: calc(2rem + 32px);
+	overflow: hidden;
+	min-height: 100vh;
+	padding: 1rem;
+	transition: 0.2s ease-in-out;
+    left: 0;
+    top: 0;
+    position: fixed;
+	.flex {
+		flex: 1 1 0%;
+	}
+	.logo {
+		margin-bottom: 1rem;
+		img {
+			width: 2rem;
+		}
+	}
+	.menu-toggle-wrap {
+		display: flex;
+		justify-content: flex-end;
+		margin-bottom: 1rem;
+		position: relative;
+		top: 0;
+		transition: 1s ease-out;
+	}
+	h3, .button .text {
+		opacity: 0;
+		transition: opacity 0.3s ease-in-out;
+	}
+	h3 {
+		color: var(--grey);
+		font-size: 0.875rem;
+		margin-bottom: 0.5rem;
+		text-transform: uppercase;
+	}
+	&.is-expanded {
+		width: 300px
+		}
+}
 </style>
